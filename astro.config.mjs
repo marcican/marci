935999a914b9ff2d4b6f18f252beb8bdd7428f49
marcican <marci3@outlook.it>
2026-06-07 15:22:55 +0200

    Setup github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://marcican.github.io",
   output: "static",
   base: "/",
   vite: {
